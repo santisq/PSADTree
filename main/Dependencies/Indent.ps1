a778e92674e9a57e392209259a1ab9a6bbb0fdc8
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-06-15 23:08:45 -0300

    Update Indent.ps1

--- main/Dependencies/Indent.ps1
+++ main/Dependencies/Indent.ps1
@@ -1,13 +1,18 @@
-﻿function Indent{
+function Indent {
 param(
     [String]$String,
     [Int]$Indent
 )
 
-$x='_';$y='|';$z='    '
+    $x='_';$y='|';$z='    '
 
-switch($Indent){
-    {$_ -eq 0}{return $String}
-    {$_ -gt 0}{return "$($z*$_)$y$x $string"}    
+    switch($Indent)
+    {
+        {$_ -eq 0}{
+            return $String
+        }
+        {$_ -gt 0}{
+            return "$($z*$_)$y$x $string"
+        }
     }
 }
