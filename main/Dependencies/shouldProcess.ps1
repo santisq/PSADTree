61e11ae8d88adc806936fee4fdf120feaf57b612
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-06-15 23:07:59 -0300

    Update shouldProcess.ps1

--- main/Dependencies/shouldProcess.ps1
+++ main/Dependencies/shouldProcess.ps1
@@ -1,4 +1,4 @@
-﻿function shouldProcess{
+function ShouldProcess {
 [cmdletbinding()]
 param(
     [string]$Name,
@@ -6,48 +6,60 @@ param(
     [string]$RecursionProperty
 )
 
-$filter="(|(distinguishedname=$Name)(samaccountname=$Name)(name=$Name))"
-switch($Name)
-{
-    'Administrators'{
-        if($RecursionProperty){
-            $object=Get-ADGroup -LDAPFilter $filter -Properties $RecursionProperty
-        }else{
-            $object=Get-ADGroup -LDAPFilter $filter
+    $filter = "(|(distinguishedname=$Name)(samaccountname=$Name)(name=$Name))"
+
+    switch($Name)
+    {
+        'Administrators'
+        {
+            if($RecursionProperty)
+            {
+                $object = Get-ADGroup -LDAPFilter $filter -Properties $RecursionProperty
+            }
+            else
+            {
+                $object = Get-ADGroup -LDAPFilter $filter
+            }
+        }
+
+        Default
+        {
+            if($RecursionProperty)
+            {
+                $object = Get-ADObject -LDAPFilter $filter -Properties $RecursionProperty
+            }
+            else
+            {
+                $object = Get-ADObject -LDAPFilter $filter
+            }
         }
     }
-    Default{
-        if($RecursionProperty){
-            $object=Get-ADObject -LDAPFilter $filter -Properties $RecursionProperty
-        }else{
-            $object=Get-ADObject -LDAPFilter $filter
+
+    if(-not $object)
+    {
+        $ErrorMessage = "Cannot find an object with identity: '{0}' under: '{1}'." -f $Name,$((Get-ADRootDSE).defaultNamingContext)
+        throw $ErrorMessage
+    }
+
+    if($object.count -gt 1)
+    {
+        throw "Multiple objects with Name '$Name' were found. Use DistinguishedName for unique output."
+    }
+
+    if($RecursionProperty)
+    {
+        [PScustomObject]@{
+            Name = $object.Name
+            UserPrincipalName = $props.UserPrincipalName
+            ObjectClass = $culture.ToTitleCase($object.ObjectClass)
+            Property = $object.$RecursionProperty
         }
+        return 
     }
-}
 
-if(!$object){
-    $eMessage="Cannot find an object with identity: '{0}' under: '{1}'." -f $Name,$((Get-ADRootDSE).defaultNamingContext)
-    throw $eMessage
-}
-if($object.count -gt 1){
-    throw "Multiple objects with Name '$Name' were found. Use DistinguishedName for unique output."
-}
-
-$culture=(Get-Culture).TextInfo
-
-if($RecursionProperty){
     [PScustomObject]@{
-        Name=$object.Name
-        UserPrincipalName=$props.UserPrincipalName
-        ObjectClass=$culture.ToTitleCase($object.ObjectClass)
-        Property=$object.$RecursionProperty
+        Name = $object.Name
+        UserPrincipalName = $props.UserPrincipalName
+        ObjectClass = $culture.ToTitleCase($object.ObjectClass)
     }
-}else{
-    [PScustomObject]@{
-        Name=$object.Name
-        UserPrincipalName=$props.UserPrincipalName
-        ObjectClass=$culture.ToTitleCase($object.ObjectClass)
-    }
-}
-
 }
