a360f2e6f3c609c5b3d3727452174d317f43b660
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-06-15 23:09:26 -0300

    Update Draw-Hierarchy.ps1

--- main/Dependencies/Draw-Hierarchy.ps1
+++ main/Dependencies/Draw-Hierarchy.ps1
@@ -1,30 +1,31 @@
-﻿function Draw-Hierarchy{
+function Draw-Hierarchy {
 param(
     [System.Collections.ArrayList]$Array
 )
 
-$Array.Reverse()
+    $Array.Reverse()
 
-for($i=0;$i -lt $Array.Count;$i++){
-
-    if(
-        $Array[$i+1] -and 
-        $Array[$i].Hierarchy.IndexOf('|_') -lt $Array[$i+1].Hierarchy.IndexOf('|_')
-    ){
-    $z=$i+1
-    $ind=$Array[$i].Hierarchy.IndexOf('|_')
-        while($Array[$z].Hierarchy[$ind] -ne '|'){
-            $string=($Array[$z].Hierarchy).ToCharArray()
-            $string[$ind]='|'
-            $string=$string -join ''
-            $Array[$z].Hierarchy=$string
-            $z++
-            if($Array[$z].Hierarchy[$ind] -eq '|'){break}
+    for($i=0;$i -lt $Array.Count;$i++)
+    {
+        if($Array[$i+1] -and $Array[$i].Hierarchy.IndexOf('|_') -lt $Array[$i+1].Hierarchy.IndexOf('|_'))
+        {
+            $z = $i+1
+            $ind = $Array[$i].Hierarchy.IndexOf('|_')
+            while($Array[$z].Hierarchy[$ind] -ne '|')
+            {
+                $string = ($Array[$z].Hierarchy).ToCharArray()
+                $string[$ind] = '|'
+                $string = $string -join ''
+                $Array[$z].Hierarchy = $string
+                $z++
+                if($Array[$z].Hierarchy[$ind] -eq '|')
+                {
+                    break
+                }
             }
         }
     }
 
-$Array.Reverse()
-return $Array
-
+    $Array.Reverse()
+    return $Array
 }
