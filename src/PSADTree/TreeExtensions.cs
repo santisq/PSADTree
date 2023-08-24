69abb9919a1f4b338101aaf07ddccefdbac39bed
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-08-24 00:02:15 -0300

    improving tree drawing algo

--- src/PSADTree/TreeExtensions.cs
+++ src/PSADTree/TreeExtensions.cs
@@ -5,9 +5,9 @@ namespace PSADTree;
 
 internal static class TreeExtensions
 {
-    private static readonly Regex s_reBoxOrNotSpace = new(
-        @"└|\S",
-        RegexOptions.Compiled);
+    // private static readonly Regex s_reBoxOrNotSpace = new(
+    //     @"└|\S",
+    //     RegexOptions.Compiled);
 
     private static readonly Regex s_reDefaultNamingContext = new(
         "(?<=,)DC=.+$",
@@ -31,33 +31,60 @@ internal static class TreeExtensions
     internal static TreeObjectBase[] ConvertToTree(
         this TreeObjectBase[] inputObject)
     {
+        int index;
+        TreeObjectBase current;
         for (int i = 0; i < inputObject.Length; i++)
         {
-            int index = inputObject[i].Hierarchy.IndexOf('└');
-
-            if (index < 0)
+            current = inputObject[i];
+            if ((index = current.Hierarchy.IndexOf('└')) == -1)
             {
                 continue;
             }
 
-            int z = i - 1;
-
-            while (!s_reBoxOrNotSpace.IsMatch(inputObject[z].Hierarchy[index].ToString()))
+            int z;
+            char[] replace;
+            for (z = i - 1; z >= 0; z--)
             {
-                char[] replace = inputObject[z].Hierarchy.ToCharArray();
+                current = inputObject[z];
+                if (!char.IsWhiteSpace(current.Hierarchy[index]))
+                {
+                    UpdateCorner(index, current);
+                    break;
+                }
+
+                replace = current.Hierarchy.ToCharArray();
                 replace[index] = '│';
-                inputObject[z].Hierarchy = new string(replace);
-                z--;
+                current.Hierarchy = new string(replace);
             }
 
-            if (inputObject[z].Hierarchy[index] == '└')
-            {
-                char[] replace = inputObject[z].Hierarchy.ToCharArray();
-                replace[index] = '├';
-                inputObject[z].Hierarchy = new string(replace);
-            }
+
+
+            // while (!s_reBoxOrNotSpace.IsMatch(inputObject[z].Hierarchy[index].ToString()))
+            // {
+            //     char[] replace = inputObject[z].Hierarchy.ToCharArray();
+            //     replace[index] = '│';
+            //     inputObject[z].Hierarchy = new string(replace);
+            //     z--;
+            // }
+
+            // if (inputObject[z].Hierarchy[index] == '└')
+            // {
+            //     char[] replace = inputObject[z].Hierarchy.ToCharArray();
+            //     replace[index] = '├';
+            //     inputObject[z].Hierarchy = new string(replace);
+            // }
         }
 
         return inputObject;
+    }
+
+    private static void UpdateCorner(int index, TreeObjectBase current)
+    {
+        if (current.Hierarchy[index] == '└')
+        {
+            char[] replace = current.Hierarchy.ToCharArray();
+            replace[index] = '├';
+            current.Hierarchy = new string(replace);
+        }
     }
 }
