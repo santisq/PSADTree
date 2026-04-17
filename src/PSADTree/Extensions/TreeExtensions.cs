8b4e47b77b4708023b52b9a233c2705a89959900
Santiago Squarzon <squarzon.santiago@gmail.com>
2026-04-17 12:33:15 -0300

    update extensions to use extension keyword

--- src/PSADTree/Extensions/TreeExtensions.cs
+++ src/PSADTree/Extensions/TreeExtensions.cs
@@ -13,153 +13,161 @@ using PSADTree.Style;
 
 namespace PSADTree.Extensions;
 
-internal static class TreeExtensions
+internal static partial class TreeExtensions
 {
+#if NETCOREAPP
+    [GeneratedRegex("(?<=,)DC=.+$", RegexOptions.Compiled)]
+    private static partial Regex GetDefaultNamingContextRegex();
+
+    private static readonly Regex s_reDefaultNamingContext = GetDefaultNamingContextRegex();
+#else
     private static readonly Regex s_reDefaultNamingContext = new(
         "(?<=,)DC=.+$",
         RegexOptions.Compiled);
-
-#if !NETCOREAPP
-    [ThreadStatic]
-    private static StringBuilder? s_sb;
 #endif
 
-    internal static string Indent(this string inputString, int indentation)
+    extension(string input)
     {
-        string corner = TreeStyle.Instance.RenderingSet.Corner;
-        int repeatCount = (4 * indentation) - 4;
-        int capacity = repeatCount + 4 + inputString.Length;
+        internal string GetDefaultNamingContext() => s_reDefaultNamingContext.Match(input).Value;
+
+#if !NETCOREAPP
+        [ThreadStatic]
+        private static StringBuilder? s_sb;
+#endif
+        internal string Indent(int indentation)
+        {
+            string corner = TreeStyle.Instance.RenderingSet.Corner;
+            int repeatCount = (4 * indentation) - 4;
+            int capacity = repeatCount + 4 + input.Length;
 
 #if NETCOREAPP
-        return string.Create(
-            capacity, (repeatCount, corner, inputString),
-            static (buffer, state) =>
-        {
-            int count = state.repeatCount;
-            buffer[..count].Fill(' ');
-            state.corner.AsSpan().CopyTo(buffer[count..]);
-            state.inputString.AsSpan().CopyTo(buffer[(count + 4)..]);
-        });
+            return string.Create(
+                capacity, (repeatCount, corner, input),
+                static (buffer, state) =>
+            {
+                int count = state.repeatCount;
+                buffer[..count].Fill(' ');
+                state.corner.AsSpan().CopyTo(buffer[count..]);
+                state.input.AsSpan().CopyTo(buffer[(count + 4)..]);
+            });
 #else
-        s_sb ??= new StringBuilder(64);
-        s_sb.Clear().EnsureCapacity(capacity);
+            s_sb ??= new StringBuilder(64);
+            s_sb.Clear().EnsureCapacity(capacity);
 
-        return s_sb
-            .Append(' ', repeatCount)
-            .Append(corner)
-            .Append(inputString)
-            .ToString();
+            return s_sb
+                .Append(' ', repeatCount)
+                .Append(corner)
+                .Append(input)
+                .ToString();
+#endif
+        }
+
+#if NETCOREAPP
+        [SkipLocalsInit]
+        private string ReplaceAt(int index, char newChar)
+            => string.Create(
+                input.Length, (input, index, newChar),
+                static (buffer, state) =>
+            {
+                state.input.AsSpan().CopyTo(buffer);
+                buffer[state.index] = state.newChar;
+            });
+#else
+        private unsafe string ReplaceAt(int index, char newChar)
+        {
+            if (input.Length > 0x200)
+            {
+                char[] chars = input.ToCharArray();
+                chars[index] = newChar;
+                return new string(chars);
+            }
+
+            char* pChars = stackalloc char[0x200];
+            fixed (char* source = input)
+            {
+                Buffer.MemoryCopy(
+                    source,
+                    pChars,
+                    0x200 * sizeof(char),
+                    input.Length * sizeof(char));
+            }
+
+            pChars[index] = newChar;
+            return new string(pChars, 0, input.Length);
+        }
 #endif
     }
 
-    internal static TreeObjectBase[] Format(
-        this TreeObjectBase[] tree)
+    extension(TreeObjectBase[] tree)
     {
-        int index;
-        RenderingSet set = TreeStyle.Instance.RenderingSet;
-        for (int i = 0; i < tree.Length; i++)
+        internal TreeObjectBase[] Format()
         {
-            TreeObjectBase current = tree[i];
-
-            if ((index = current.Hierarchy.IndexOf(set.UpRight)) == -1)
+            int index;
+            RenderingSet set = TreeStyle.Instance.RenderingSet;
+            for (int i = 0; i < tree.Length; i++)
             {
-                continue;
-            }
+                TreeObjectBase current = tree[i];
 
-            for (int z = i - 1; z >= 0; z--)
-            {
-                current = tree[z];
-                string hierarchy = current.Hierarchy;
-
-                if (char.IsWhiteSpace(hierarchy[index]))
+                if ((index = current.Hierarchy.IndexOf(set.UpRight)) == -1)
                 {
-                    current.Hierarchy = hierarchy.ReplaceAt(index, set.Vertical);
                     continue;
                 }
 
-                if (hierarchy[index] == set.UpRight)
+                for (int z = i - 1; z >= 0; z--)
                 {
-                    current.Hierarchy = hierarchy.ReplaceAt(index, set.VerticalRight);
-                }
+                    current = tree[z];
+                    string hierarchy = current.Hierarchy;
 
-                break;
-            }
-        }
+                    if (char.IsWhiteSpace(hierarchy[index]))
+                    {
+                        current.Hierarchy = hierarchy.ReplaceAt(index, set.Vertical);
+                        continue;
+                    }
 
-        return tree;
-    }
+                    if (hierarchy[index] == set.UpRight)
+                    {
+                        current.Hierarchy = hierarchy.ReplaceAt(index, set.VerticalRight);
+                    }
 
-#if NETCOREAPP
-    [SkipLocalsInit]
-    private static string ReplaceAt(this string input, int index, char newChar)
-        => string.Create(
-            input.Length, (input, index, newChar),
-            static (buffer, state) =>
-        {
-            state.input.AsSpan().CopyTo(buffer);
-            buffer[state.index] = state.newChar;
-        });
-#else
-    private static unsafe string ReplaceAt(this string input, int index, char newChar)
-    {
-        if (input.Length > 0x200)
-        {
-            char[] chars = input.ToCharArray();
-            chars[index] = newChar;
-            return new string(chars);
-        }
-
-        char* pChars = stackalloc char[0x200];
-        fixed (char* source = input)
-        {
-            Buffer.MemoryCopy(
-                source,
-                pChars,
-                0x200 * sizeof(char),
-                input.Length * sizeof(char));
-        }
-
-        pChars[index] = newChar;
-        return new string(pChars, 0, input.Length);
-    }
-#endif
-
-    internal static IEnumerable<Principal> ToSafeSortedEnumerable<TPrincipal>(
-        this TPrincipal principal,
-        Func<TPrincipal, PrincipalSearchResult<Principal>> selector,
-        PSCmdlet cmdlet)
-        where TPrincipal : Principal
-    {
-        List<Principal> principals = [];
-        using PrincipalSearchResult<Principal> search = selector(principal);
-        using IEnumerator<Principal> enumerator = search.GetEnumerator();
-
-        while (true)
-        {
-            try
-            {
-                if (!enumerator.MoveNext())
-                {
                     break;
                 }
+            }
 
-                principals.Add(enumerator.Current);
-            }
-            catch (Exception _) when (_ is PipelineStoppedException or FlowControlException)
-            {
-                throw;
-            }
-            catch (Exception exception)
-            {
-                cmdlet.WriteError(exception.ToEnumerationFailure(principal));
-            }
+            return tree;
         }
-
-        return principals
-            .OrderBy(static e => e.StructuralObjectClass == "group")
-            .ThenBy(static e => e, PSADTreeComparer.Value);
     }
 
-    internal static string GetDefaultNamingContext(this string distinguishedName) =>
-        s_reDefaultNamingContext.Match(distinguishedName).Value;
+    extension<TPrincipal>(TPrincipal principal) where TPrincipal : Principal
+    {
+        internal IEnumerable<Principal> ToSafeSortedEnumerable(
+            Func<TPrincipal, PrincipalSearchResult<Principal>> selector,
+            PSCmdlet cmdlet)
+        {
+            List<Principal> principals = [];
+            using PrincipalSearchResult<Principal> search = selector(principal);
+            using IEnumerator<Principal> enumerator = search.GetEnumerator();
+
+            while (true)
+            {
+                try
+                {
+                    if (!enumerator.MoveNext()) break;
+
+                    principals.Add(enumerator.Current);
+                }
+                catch (Exception _) when (_ is PipelineStoppedException or FlowControlException)
+                {
+                    throw;
+                }
+                catch (Exception exception)
+                {
+                    cmdlet.WriteError(exception.ToEnumerationFailure(principal));
+                }
+            }
+
+            return principals
+                .OrderBy(static e => e.StructuralObjectClass == "group")
+                .ThenBy(static e => e, PSADTreeComparer.Value);
+        }
+    }
 }
